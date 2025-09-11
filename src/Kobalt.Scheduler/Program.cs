b3b3bbd0f0abdcb5b707a5a26119eb662b5c4b23
Bas Pennings <baspster@gmail.com>
2025-09-11 19:58:47 +0200

    Cleanup and try out FixedWidth package.

---
+++ src/Kobalt.Scheduler/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Quartz;
+
+var builder = Host.CreateDefaultBuilder()
+    .ConfigureServices((opt, services) =>
+    {
+        services.AddQuartz();
+        services.AddQuartzHostedService(opt =>
+        {
+            opt.WaitForJobsToComplete = true;
+        });
+    })
+    .Build();
+
+var schedulerFactory = builder
+    .Services
+    .GetRequiredService<ISchedulerFactory>();
+
+var scheduler = await schedulerFactory.GetScheduler();
+
+var job = JobBuilder.Create<HelloJob>()
+    .WithIdentity("job1", "group1")
+    .Build();
+
+var trigger = TriggerBuilder.Create()
+    .WithIdentity("myTrigger", "group1")
+    .StartNow()
+    .Build();
+
+await scheduler.ScheduleJob(job, trigger);
+
+await builder.RunAsync();
+
+internal class HelloJob : IJob
+{
+    private static int count = 0;
+    
+    public async Task Execute(IJobExecutionContext context)
+    {
+        HelloJob.count += 1;
+        
+        Console.WriteLine($"Hello World! ({HelloJob.count}x)");
+        
+        var oldTrigger = context.Trigger;
+        var newTrigger = TriggerBuilder.Create()
+            .ForJob(context.JobDetail)
+            .WithIdentity(
+                $"{oldTrigger.Key.Name}-retry", 
+                oldTrigger.Key.Group)
+            .StartAt(DateTime.UtcNow.AddSeconds(10))
+            .Build();
+
+        await context.Scheduler.ScheduleJob(newTrigger);
+    }
+}
