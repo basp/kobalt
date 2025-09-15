a99a7a365d82c79dc8392a9aabbeaa6dcba86a1e
Bas Pennings <baspster@gmail.com>
2025-09-15 22:25:24 +0200

    WWB parser.

--- src/Kobalt.Cmd/Program.cs
+++ src/Kobalt.Cmd/Program.cs
@@ -1,7 +1,10 @@
 ﻿using Kobalt.Belastingdienst;
+using Kobalt.SVB;
 using Microsoft.Extensions.Logging;
 using Setl;
+using Setl.Operations;
 using Setl.Pipelines;
+#pragma warning disable CS8321 // Local function is declared but never used
 
 var logger = LoggerFactory.Create(builder =>
     {
@@ -14,14 +17,75 @@ var logger = LoggerFactory.Create(builder =>
     })
     .CreateLogger<Program>();
 
-AlleenverdienersExample(logger);
+var executor = new SingleThreadedPipelineExecutor(logger);
+
+const string path = @"D:\temp\SVB\SVBWWB65PLUS00002_3.txt";
+
+var extract = new SimpleExtract(() =>
+    {
+        var parser = new WWB65PlusParser();
+        var stream = File.OpenRead(path);
+        return parser.Parse(stream);
+    },
+    logger);
+
+var aggregate = new WWB65PlusAggregationOperation(logger);
+
+var load = new SimpleTransform(row =>
+    {
+        var bericht = row.ToObject<WWB65PlusAggregationOperation.Gemeentebericht>();
+        var gemeente = bericht.Gemeente.ToObject<WWB65PlusParser.Gemeenterecord>();
+        var details = 
+            bericht.Details
+                .Select(x => x.ToObject<WWB65PlusParser.Detailrecord>())
+                .ToList();
+        var tellingen = bericht.Tellingen.ToObject<WWB65PlusParser.Tellingenrecord>();
+        logger.LogInformation(
+            "{Recordcode}: {Gemeente}", 
+            gemeente.Recordcode, 
+            gemeente.Gemeentecode);
+        logger.LogInformation("DTR: {Count}", details.Count);
+        logger.LogInformation(
+            "{Recordcode}: {Gemeente} (Huishoudens: {Huishoudens}, Gerechtigden: {Gerechtigden})", 
+            tellingen.Recordcode,
+            tellingen.Gemeentecode,
+            tellingen.TotaalAantalHuishoudens,
+            tellingen.TotaalAantalGerechtigden);
+        return row;
+    },
+    logger);
+
+var process = new SimpleProcess(init =>
+    {
+        init.Register(extract);
+        init.Register(aggregate);
+        init.Register(load);
+    }, 
+    logger,
+    executor);
+
+process.Execute();
 
 return;
+
+static void WWB65PlusExample(ILogger logger)
+{
+    const string path = @"D:\temp\SVB\SVBWWB65PLUS00002_3.txt";
+    var parser = new WWB65PlusParser();
+    var stream = File.OpenRead(path);
+    var records = parser.Parse(stream).ToList();
+    foreach (var record in records)
+    {
+        // logger.LogInformation("Record: {Record}", record);
+        Console.WriteLine(
+            "[Recordcode] {0}", 
+            record["Recordcode"]);
+    }
+}
 
 static void AlleenverdienersExample(ILogger logger)
 {
     const string path = @"D:\temp\BD\avd_corrupt.csv";
-    
     var parser = new AlleenverdienersParser(logger);
     var stream = File.OpenRead(path);
     var records = parser.Parse(stream);
@@ -34,12 +98,9 @@ static void AlleenverdienersExample(ILogger logger)
     }   
 }
 
-#pragma warning disable CS8321 // Local function is declared but never used
 static void BankrekeningenExample(ILogger logger)
-#pragma warning restore CS8321 // Local function is declared but never used
 {
     const string path = @"D:\temp\BD\INL_VINLBUR010_goed_20240516.txt";
-
     var executor = new SingleThreadedPipelineExecutor(logger);
     var stream = File.OpenRead(path);
     var process = new SimpleProcess(
@@ -49,6 +110,5 @@ static void BankrekeningenExample(ILogger logger)
         }, 
         logger, 
         executor);
-
     process.Execute();    
 }
---
+++ src/Kobalt/SVB/WWB65PlusAggregationOperation.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.Logging;
+using Setl;
+using Setl.Operations;
+
+namespace Kobalt.SVB;
+
+public class WWB65PlusAggregationOperation : AbstractOperation
+{
+    private static class WellKnownRecordcodes
+    {
+        public const string Bericht = "BER";
+        public const string Gemeente = "GEM";
+        public const string Detail = "DTR";
+        public const string Tellingen = "TPG";
+    }
+
+    private Row gemeente = [];
+    private List<Row> details = [];
+    
+    public WWB65PlusAggregationOperation(ILogger logger) : base(logger)
+    {
+    }
+
+    public override IEnumerable<Row> Execute(IEnumerable<Row> rows)
+    {
+        foreach (var row in rows)
+        {
+            var recordcode = row["Recordcode"];
+            switch (recordcode)
+            {
+                case WellKnownRecordcodes.Bericht:
+                    break;
+                case WellKnownRecordcodes.Gemeente:
+                    this.gemeente = row.Clone();
+                    break;
+                case WellKnownRecordcodes.Detail:
+                    this.details.Add(row.Clone());
+                    break;
+                case WellKnownRecordcodes.Tellingen:
+                    var bericht = new Gemeentebericht
+                    {
+                        Gemeente = this.gemeente,
+                        Details = this.details,
+                        Tellingen = row.Clone(),
+                    };
+                    
+                    this.gemeente = [];
+                    this.details = [];
+                    
+                    yield return Row.FromObject(bericht);
+                    break;
+            }
+        }
+    }
+
+    public class Gemeentebericht
+    {
+        public Row Gemeente { get; set; } = [];
+        public List<Row> Details { get; set; } = [];
+        public Row Tellingen { get; set; } = [];
+    }
+}
